70742d8515db49c0bf6359c279fd2e687109f318
Jayden Kohlmeyer - STUDENT <l00184357@atu.ie>
2026-02-13 10:24:56 +0000

    fixed dataAccess and models folders

---
+++ TourCompany.DataAccess/DataAccess/TourDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TourCompany.Models.Models;
+
+
+namespace TourCompany.DataAccess.DataAccess
+{
+    public class TourDBContext : DbContext
+    {
+        public TourDBContext(DbContextOptions<TourDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
