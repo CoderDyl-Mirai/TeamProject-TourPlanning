90217d98cf4e0b067804e51fd7fc3773d6cd9c82
Jayden Kohlmeyer - STUDENT <l00184357@atu.ie>
2026-03-05 16:05:26 +0000

    Created Details Branch

---
+++ TourCompany.DataAccess/Migrations/20260305160144_05.03.26.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TourCompany.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class _050326 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
